229290419aac411d86ad0dbad878bf0e726f29b1
smaciejew <smaciejew@gmail.com>
2012-05-20 23:51:54 +0000

    Touch + panel

--- src/ooviewController.js
+++ src/ooviewController.js
@@ -21,7 +21,8 @@ viewController = function(id, mesh_name) {
   this.slowRotationY = 0;
   this.home = false;
   this.maxDimension = 0;
-
+  this.CAMERA_MOVE = 5;
+  
   this.width = 300;
   this.height = 300;
 
@@ -67,9 +68,9 @@ viewController.prototype.init = function () {
 	this.renderer.domElement.addEventListener('mousedown', this.onMouseDown.bind(this), false);
 	this.renderer.domElement.addEventListener('DOMMouseScroll', this.onMouseScroll.bind(this), false);
 	this.renderer.domElement.addEventListener('mousewheel', this.onMouseScroll.bind(this), false);
-//	this.renderer.domElement.addEventListener('touchstart', onTouchStart, false);
-//	this.renderer.domElement.addEventListener('touchmove', onTouchMove, false);
-//	this.renderer.domElement.addEventListener('touchend', onTouchEnd, false);
+	this.renderer.domElement.addEventListener('touchstart', this.onTouchStart.bind(this), false);
+	this.renderer.domElement.addEventListener('touchmove', this.onTouchMove.bind(this), false);
+	this.renderer.domElement.addEventListener('touchend', this.onTouchEnd.bind(this), false);
 	this.renderer.domElement.addEventListener('contextmenu', this.onContextMenu.bind(this), false);
 
 
@@ -105,21 +106,83 @@ viewController.prototype.createPanel = function () {
 	container.className = "panel";
 	container.style.cssText = 'z-index: 10; top: 0px; float: left; position: relative;';
 	
-	var home_button = document.createElement( 'button' );
-	home_button.innerHTML="Reset view";
-	home_button.style.cssText = 'color:#000000';
-
-	home_button.onclick = this.Home;
-	container.appendChild( home_button );
 	
+
+	var tab1 = document.createElement('table');
+	var tab2 = document.createElement('table');
+	var tr1 = document.createElement('tr');
+	var tr2 = document.createElement('tr');
+	var td = document.createElement('td');
+	var arrbutton = document.createElement('button');
 	var divslider = document.createElement('div');
+	var divsliderinput = document.createElement('input');
+	
 	divslider.className = "slider";
 	divslider.id = "slider-vertical-"+this.id;
-	var divsliderinput = document.createElement('input');
+	
 	divsliderinput.id = "slider-vertical-input-"+this.id;
 	divsliderinput.className = "slider-input";
 	divslider.appendChild(divsliderinput);
-	container.appendChild(divslider);
+	
+	td.appendChild(divslider);
+	tr1.appendChild(td);
+	
+	td = document.createElement('td');
+	tr2.appendChild(td);
+	td = document.createElement('td');
+	arrbutton.onclick = this.moveCamera;
+	arrbutton.direction = 'up'; 
+	arrbutton.innerHTML = "&uarr;";
+	td.appendChild(arrbutton);
+	tr2.appendChild(td);
+	td = document.createElement('td');
+	tr2.appendChild(td);
+	tab2.appendChild(tr2);
+	tr2 = document.createElement('tr');
+	td = document.createElement('td');
+	arrbutton = document.createElement('button');
+	arrbutton.onclick = this.moveCamera;
+	arrbutton.innerHTML = "&larr;";
+	arrbutton.direction = 'left';
+	td.appendChild(arrbutton);
+	tr2.appendChild(td);
+	td = document.createElement('td');
+	tr2.appendChild(td);
+	td = document.createElement('td');
+	arrbutton = document.createElement('button');
+	arrbutton.onclick = this.moveCamera;
+	arrbutton.innerHTML = "&rarr;";
+	arrbutton.direction = 'right';
+	td.appendChild(arrbutton);
+	tr2.appendChild(td);
+	tab2.appendChild(tr2);
+	tr2 = document.createElement('tr');
+	td = document.createElement('td');
+	tr2.appendChild(td);
+	td = document.createElement('td');
+	arrbutton = document.createElement('button');
+	arrbutton.onclick = this.moveCamera;
+	arrbutton.innerHTML = "&darr;";
+	arrbutton.direction = 'down';
+	td.appendChild(arrbutton);
+	tr2.appendChild(td);
+	td = document.createElement('td');
+	tr2.appendChild(td);
+	tab2.appendChild(tr2);
+	
+	td = document.createElement('td');
+	td.appendChild(tab2);
+	var home_button = document.createElement( 'button' );
+	home_button.innerHTML="Reset view";
+	home_button.style.cssText = 'color:#000000';
+	home_button.onclick = this.Home;
+	td.appendChild(home_button);
+	tr1.appendChild(td);
+	tab1.appendChild(tr1);
+	
+	container.appendChild( tab1);
+	
+
 	
 	this.vslider = new Slider(divslider, divsliderinput, "vertical");
 	this.vslider.onchange = this.onSliderChange.bind(this);
@@ -290,7 +353,7 @@ viewController.prototype.onSliderChange = function (event) {
 }
 
 
-/*
+
 viewController.prototype.onTouchStart = function (event) {
 	
 	var vc = this;
@@ -308,29 +371,31 @@ viewController.prototype.onTouchStart = function (event) {
 }
 
 
-function onTouchMove(event) {
+viewController.prototype.onTouchMove = function (event) {
 	
+	var vc = this;
 	event = event ? event: document.event;
 	
 	event.preventDefault();
 	if(event.touches.length == 1) {
-		mouseX = event.touches[0].pageX - windowHalfX;
-		mouseY = event.touches[0].pageY - windowHalfY;
-		targetRotationX = mouseDownRotationX + (mouseX - mouseDownX)*0.05;
-		targetRotationY = mouseDownRotationY + (mouseY - mouseDownY)*0.05;
+		vc.mouseX = event.touches[0].pageX - vc.windowHalfX;
+		vc.mouseY = event.touches[0].pageY - vc.windowHalfY;
+		vc.targetRotationX = vc.mouseDownRotationX + (vc.mouseX - vc.mouseDownX)*0.05;
+		vc.targetRotationY = vc.mouseDownRotationY + (vc.mouseY - vc.mouseDownY)*0.05;
 	}
 	
 }
 
-function onTouchEnd(event) {
+viewController.prototype.onTouchEnd = function (event) {
 	
+	var vc = this;
 	event = event ? event: document.event;
 	
 	event.preventDefault();
-	mesh.doubleSided = true;
-	mesh.material.wireframe = false;
+	vc.mesh.doubleSided = true;
+	vc.mesh.material.wireframe = false;
 }
-*/
+
 
 
 viewController.prototype.onContextMenu = function (event) {
@@ -363,11 +428,10 @@ function Inertia() {
 
 viewController.prototype.Home = function() {
 
-	var vc = vc_byid[event.target.parentNode.parentNode.id];
-
+	var vc = vc_byid[this.parentNode.parentNode.parentNode.parentNode.parentNode.id];
 	vc.targetRotationX = vc.targetRotationY = vc.mouseDownRotationX = vc.mouseDownRotationY = vc.mouseDownX = vc.mouseDownY = vc.mouseX = vc.mouseY = vc.slowRotationX = vc.slowRotationY = 0;
-	//mesh.rotation.setRotationFromMatrix(new THREE.Matrix4());
-//	vslider.setValue(maxDimension*0.25);
+	vc.mesh.rotation.setRotationFromMatrix(new THREE.Matrix4());
+	vc.vslider.setValue(vc.maxDimension*0.25);
 	vc.mesh.geometry.computeBoundingBox();
 	var box = vc.mesh.geometry.boundingBox;
 	vc.camera.position.x = box.x[0] + (box.x[1]-box.x[0])/2;
@@ -376,17 +440,17 @@ viewController.prototype.Home = function() {
 
 }
 
-/*
-function moveCamera(direction) {
-	
 
-	switch(direction) {
+viewController.prototype.moveCamera = function(direction) {
+
+	var vc = vc_byid[this.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.id];//	alert("TU");
+	switch(this.direction) {
 		
-		case "up": camera.position.y -= CAMERA_MOVE; break;
-		case "down": camera.position.y += CAMERA_MOVE; break;
-		case "left": camera.position.x += CAMERA_MOVE; break;
-		case "right": camera.position.x -= CAMERA_MOVE; break;
+		case "up": vc.camera.position.y -= vc.CAMERA_MOVE; break;
+		case "down": vc.camera.position.y += vc.CAMERA_MOVE; break;
+		case "left": vc.camera.position.x += vc.CAMERA_MOVE; break;
+		case "right": vc.camera.position.x -= vc.CAMERA_MOVE; break;
 		default: break;
 	}
 }
-*/
+
